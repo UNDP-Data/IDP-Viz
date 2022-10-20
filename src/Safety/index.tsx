05cc993f15b0a452b571548f5d0bf9ff6fc5b0d5
Mustafa Saifee <29223820+mustafasaifee42@users.noreply.github.com>
2022-10-20 19:16:12 +0300

    Update Safety and Education

--- src/Safety/index.tsx
+++ src/Safety/index.tsx
@@ -31,17 +31,14 @@ export const SafetyEl = () => (
     <div className='flex-wrap flex-div max-width flex-space-between margin-bottom-07' style={{ alignItems: 'stretch' }}>
       <DivEl>
         <p className='undp-typography'>
-          <span className='bold'>While the majority of IDPs, now feel equally safe or safer than before, IDP women living in shelters remain largely unsafe</span>
-          .  After displacement, no significant difference in the safety feeling of IDPs and hosts and across sexes. However, 3% of IDPs in the sample live in either collective or make-shift shelters. For these IDPs, the probability of feeling equally safe or safer now than before they left home is 18% less than for IDPs living either in their homes or with someone else. Women are especially vulnerable as they only have a 43% probability of feeling equally safe or safer if they live in a shelter. In comparison, the probability is 78% for women living in a home they own or rent.
+          <span className='bold'>The vast majority of IDPs (75%) feel as safe now as they did before</span>
+          .  Only 6 IDPs feel less safe now than they did at home.  Thus, compared to before, on average, IDPs feel 16% more safe than hosts after displacement.
         </p>
       </DivEl>
       <DivEl className='margin-bottom-05' style={{ backgroundColor: 'var(--gray-100)', padding: 'var(--spacing-05)' }}>
         <h6 className='undp-typography' style={{ color: 'var(--blue-500)' }}>
           Probability for IDPs to feel equally safe or safer than before based on housing situation (higher is better)
         </h6>
-        <p className='margin-bottom-07 bold undp-typography'>
-          Women living in shelter feel much less safe than the other IDPs living in other types of accomodation
-        </p>
         <div className='flex-div' style={{ fontSize: '0.875rem' }}>
           <div className='flex-div flex-vert-align-center flex-gap-02'>
             <ColorBox color='var(--men-highlight)' />
