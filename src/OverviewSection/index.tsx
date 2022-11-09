3ee3e310e0f4c92db80ed386b95c39f7f8f1b28f
Mustafa Saifee <29223820+mustafasaifee42@users.noreply.github.com>
2022-11-09 13:56:08 -0500

    Update index.tsx

--- src/OverviewSection/index.tsx
+++ src/OverviewSection/index.tsx
@@ -128,7 +128,7 @@ export const OverviewEl = () => (
       </CardEl>
       <CardEl className='stat-card' width='calc(33.33% - 1.33rem)'>
         <h2>3%</h2>
-        <p>People in host communities were were hosting 5 IDPs or more</p>
+        <p>People in host communities were hosting 5 IDPs or more</p>
       </CardEl>
       <CardEl className='stat-card' width='calc(33.33% - 1.33rem)'>
         <h2>69%</h2>
