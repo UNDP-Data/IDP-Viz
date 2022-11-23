1936a2cf13d28d4ab291f46ce664da43c4fca6a6
Mustafa Saifee <29223820+mustafasaifee42@users.noreply.github.com>
2022-11-23 21:15:42 +0200

    Update index.tsx

--- src/Education/index.tsx
+++ src/Education/index.tsx
@@ -29,8 +29,8 @@ const ColorBox = styled.div<ColorBoxProps>`
 
 export const EducationEl = () => (
   <div>
-    <div className='flex-wrap flex-div max-width flex-space-between margin-bottom-07' style={{ alignItems: 'stretch' }}>
-      <DivEl width='calc(50% - 1rem)'>
+    <div className='flex-wrap flex-div max-width flex-space-between margin-bottom-03' style={{ alignItems: 'stretch' }}>
+      <DivEl width='100%'>
         <p className='undp-typography'>
           Again, when it comes to school attendance and breaks in education, for both outcomes,
           {' '}
@@ -38,6 +38,8 @@ export const EducationEl = () => (
           . Among IDPs, younger kids are more likely to stop going to school at some point. Boys and girls are also as likely to face breaks and to be attending school.
         </p>
       </DivEl>
+    </div>
+    <div className='flex-wrap flex-div max-width flex-space-between margin-bottom-07' style={{ alignItems: 'stretch' }}>
       <DivEl width='calc(50% - 1rem)' className='margin-bottom-05' style={{ backgroundColor: 'var(--gray-100)', padding: 'var(--spacing-05)' }}>
         <h6 className='undp-typography' style={{ color: 'var(--blue-500)' }}>
           Probability of having experienced breaks in schooling (lower is better)
@@ -64,8 +66,6 @@ export const EducationEl = () => (
           marks={['Host', 'IDP']}
         />
       </DivEl>
-    </div>
-    <div className='flex-wrap flex-div max-width flex-space-between margin-bottom-07' style={{ alignItems: 'stretch' }}>
       <DivEl width='calc(50% - 1rem)' className='margin-bottom-05' style={{ backgroundColor: 'var(--gray-100)', padding: 'var(--spacing-05)' }}>
         <h6 className='undp-typography' style={{ color: 'var(--blue-500)' }}>
           Probability of attending school (higher is better)
@@ -91,16 +91,6 @@ export const EducationEl = () => (
           labelSuffix=''
           marks={['Host', 'IDP']}
         />
-      </DivEl>
-      <DivEl width='calc(50% - 1rem)'>
-        <p className='undp-typography'>
-          <span className='bold'>However, IDP parents appear successful in getting their children back into the education system</span>
-          {' '}
-          as no significant difference in attendance was found between hosts and IDPs at time of the survey. However, in both groups,
-          {' '}
-          <span className='bold'>girls are 8% less likely to be attending school</span>
-          . IDP girls who experienced breaks in education have the lowest attendance rate of all: 16% of them were out of school at the time of the survey.
-        </p>
       </DivEl>
     </div>
   </div>
